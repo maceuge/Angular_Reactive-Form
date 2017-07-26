e85f1c7acd493ade84fa5bcf8232b40e7b001136
Eugenio Vorontsov <maceuge@icloud.com>
2017-07-25 22:35:36 -0300

    Validación por subscribe y mensajes individuales. Validación opcional

--- src/app/reactive-form/reactive_form.component.ts
+++ src/app/reactive-form/reactive_form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import {FormGroup, FormBuilder, Validators, AbstractControl} from '@angular/forms';
 import { Customer } from './customer';
 
 @Component({
@@ -13,19 +13,90 @@ export class ReactiveFormComponent implements OnInit{
   customerForm: FormGroup;
   customer: Customer = new Customer();
 
+  emailMessage: string;
+
+  private validationMessages = {
+    required: 'Este campo es obligatorio',
+    pattern: 'El email no es valido'
+  }
+
+  constructor (private fb: FormBuilder) {}
+
   ngOnInit (): void {
-    this.customerForm = new FormGroup({
-      firstName: new FormControl(),
-      lastName: new FormControl(),
-      email: new FormControl(),
-      sendCatalog: new FormControl(true)
-    })
+
+    this.customerForm = this.fb.group({
+      firstName: ['', [Validators.required, Validators.minLength(3)]],
+      lastName: ['', [Validators.required, Validators.maxLength(15)]],
+      email: ['', [Validators.required, Validators.pattern('[a-z0-9._%+-]+@[a-z0-9,-]+')]],
+      phone: '',
+      notification: 'email',
+      sendCatalog: true
+    });
+
+    this.customerForm.get('notification').valueChanges
+        .subscribe(value => console.log(value));
+
+    const emailControl = this.customerForm.get('email');
+          emailControl.valueChanges.subscribe(value => this.setMessage(emailControl));
+
+  }
+
+  setMessage (c: AbstractControl): void {
+    this.emailMessage = '';
+      if ((c.touched || c.dirty) && c.errors) {
+          this.emailMessage = Object.keys(c.errors)
+            .map(key => this.validationMessages[key]).join(' ');
+      }
   }
 
   save () {
-    console.log();
-    console.log('Guardado: ' + this.customerForm);
-    this.customer.firstName = '';
+    console.log(this.customerForm);
+    console.log('Guardado: ' + JSON.stringify(this.customerForm.value));
+    // this.clearForm();
+    // this.customerForm.reset();
+    // this.customerForm.clearValidators();
+  }
+
+  setNotification (notifVia: string) :void {
+    const phoneContol = this.customerForm.get('phone');
+
+      if (notifVia === 'text') {
+         phoneContol.setValidators(Validators.required);
+      } else {
+         phoneContol.clearValidators();
+      }
+
+      phoneContol.updateValueAndValidity();
+  }
+
+  fillFormWithData () {
+    document.getElementById('firstName').focus()
+
+    if (document.getElementById('firstName').focus()) {
+      document.getElementById('firstName').blur();
+      document.getElementById('lastName').focus();
+    }
+
+    // document.getElementById('lastName').focus();
+    // document.getElementById('email').focus();
+
+    this.customerForm.patchValue({
+      firstName: 'Jason',
+      lastName: 'Bourne',
+      email: 'jason@gmail',
+      sendCatalog: false
+    });
+  }
+
+  clearForm () {
+    this.customerForm.setValue({
+      firstName: '',
+      lastName: '',
+      email: '',
+      phone: '',
+      notification: 'email',
+      sendCatalog: false
+    });
   }
 
 }
